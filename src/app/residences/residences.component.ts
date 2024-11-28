c4060f371d056e9a34f842d42dbd44dfeef383de
oussema.sellami <49963438+oussemasellami@users.noreply.github.com>
2024-11-28 11:12:53 +0100

    atelier

--- src/app/residences/residences.component.ts
+++ src/app/residences/residences.component.ts
@@ -14,6 +14,32 @@ export class ResidencesComponent {
      {id:3,"name": "El Arij", "address":"Rades","image":"../../assets/images/residence3.jpg", status: "Vendu"},
      {id:4,"name": "El Anber","address":"inconnu", "image":"../../assets/images/residence3.jpg", status: "En Construction"}
    ];
- 
+ listfavorie:Residence[]=[]
+   showalertinconnu(residence:Residence){
+if(residence.address === "inconnu"){
+  alert(`L'adresse"${residence.address}"est inconnu`)
+}else{
+  alert(`L'adresse"${residence.address}":"${residence.name}"existe`)
+}
 
+   }
+   addtofavorie(residence:Residence){
+    const index=this.listfavorie.findIndex(r=>r.id==residence.id)
+if (index>-1){
+this.listfavorie.splice(index,1)
+}else{
+  this.listfavorie.push(residence)
+  console.log("liste favorie : "+JSON.stringify(this.listfavorie))
+}
+   }
+
+   isverif(residence:Residence){
+    return this.listfavorie.some(r=>r.id===residence.id)
+
+   }
+
+   searchbyname(){
+    return this.listResidences.filter(r=>r.name.toLowerCase()
+    .includes(this.searchname.toLowerCase()))
+   }
 }
